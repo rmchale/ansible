c5e9907f27d0373c4d8b44eb16a084e234c0f3df
Ryan McHale <ryan.mchale@networkedinsights.com>
2017-06-22 13:25:08 -0500

    masters

--- storm/stormup.sh
+++ storm/stormup.sh
@@ -1,1 +1,3 @@
-ansible-playbook -i `gcloud compute instances list --project=cloud9-dev |grep zookeeper |awk '{ print $5 }'`, storm.yaml
+ansible-playbook -i `gcloud compute instances list --project=cloud9-dev |grep stormmaster |awk '{ print $5 }'`, zookeeper.yaml
+ansible-playbook -i `gcloud compute instances list --project=cloud9-dev |grep stormworker-1 |awk '{ print $5 }'`, stormworker.yaml
+ansible-playbook -i `gcloud compute instances list --project=cloud9-dev |grep stormmaster |awk '{ print $5 }'`, master.yaml
