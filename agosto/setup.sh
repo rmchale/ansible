bfa812a611051abc1c9320270f73d9687dcd1da8
Ryan McHale <ryan.mchale@networkedinsights.com>
2017-10-04 23:07:31 -0500

    vw

--- agosto/setup.sh
+++ agosto/setup.sh
@@ -4,4 +4,5 @@ gcloud init --project cloud9-analytics
 gcloud source repos clone modified-fasttext --project=cloud9-analytics
 gsutil -m cp -R gs://analytics-nlp/agosto_test_data/fasttext_formatted .
 gsutil -m cp -R gs://analytics-nlp/agosto_test_data/fasttext_formatted_multi . 
-gsutil -m cp -R gs://analytics-nlp/model/ft .
+gsutil -m cp -R gs://ni-ml/model/ft/july_5_percent_agosto_settings.bin .
+gsutil -m cp -R gs://ni-ml/model/vw/july_5_percent_agosto_settings.model .
